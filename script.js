339a52a899d6a32eb1a236e78ca47df7cea80bd8
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-17 16:22:12 +0100

    Add js to turn off the light

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ const imgOffEl = document.querySelector('.d-block')
 const imgOnEl = document.querySelector('.d-none')
 
 btnEl.addEventListener('click', function (){
-    imgOffEl.classList.remove('d-block')
-    imgOffEl.classList.add('d-none')
-    imgOnEl.classList.remove('d-none')
-    imgOnEl.classList.add('d-block')
+    if (imgOffEl.classList.contains('d-block')) {
+            imgOffEl.classList.remove('d-block')
+            imgOffEl.classList.add('d-none')
+            imgOnEl.classList.remove('d-none')
+            imgOnEl.classList.add('d-block')
+        
+            btnEl.innerHTML = ('Spegni')
+        
+    } else {
+        
+            imgOnEl.classList.remove('d-block')
+            imgOnEl.classList.add('d-none')
+            imgOffEl.classList.remove('d-none')
+            imgOffEl.classList.add('d-block')
+        
+            btnEl.innerHTML = ('Accendi')
+        }
 })
+
+
